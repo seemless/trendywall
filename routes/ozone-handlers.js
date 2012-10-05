9b4831a8734d97b27507110ccd993433bf859782
Matt <matthew.christopher.smith@gmail.com>
2012-10-05 15:55:17 -0400

    added rudimentary handler for adding ozone stuff. Not there yet, but proves some Async refactoring

--- routes/ozone-handlers.js
+++ routes/ozone-handlers.js
@@ -1,11 +1,23 @@
+var twitterHandlers = require("./twitter-handlers.js");
+
+
 var OzoneHandlers = function() {
-    var firstService= function(queryParam) {
-        var html = "<html><body>"+queryParam+"</body></html>"; 
-        return html;
+    var tweets= function(req, res, callback) {
+    	twitterHandlers.getTweets(req.params.query, function(err,reply){
+    		if(err !== null){
+    			console.log("Error on OzoneHandlers.tweets()" + err);
+    			res.end("sorry 404");
+    		}
+    		else{
+    			var html = "<html><body>adding some stuff."+reply+"</body></html>"; 
+        		res.end(html);
+        	}
+    	});
+        
     }
 
     return {
-        firstService: firstService,
+        tweets: tweets,
        
     }
 }();
