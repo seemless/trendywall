ce08e4cdc5e1ce7d1626ed543050c3c02cb953de
Matt <matthew.christopher.smith@gmail.com>
2012-10-09 15:52:45 -0400

    added breaking news handler

--- routes/ozone-handlers.js
+++ routes/ozone-handlers.js
@@ -2,8 +2,8 @@ var twitterHandlers = require("./twitter-handlers.js");
 
 
 var OzoneHandlers = function() {
-    var tweets= function(req, res, callback) {
-    	twitterHandlers.getTweets(req.params.query, function(err,reply){
+    var tweets= function(req, res) {
+    	twitterHandlers.getHtmlTweets(req.params.query, function(err,reply){
     		if(err !== null){
     			console.log("Error on OzoneHandlers.tweets()" + err);
     			res.end("sorry 404");
@@ -16,9 +16,29 @@ var OzoneHandlers = function() {
         
     }
 
+    var breakingNews = function(req, res) {
+
+        twitterHandlers.getBreakingNews(function(err, reply){
+
+            if(err !== null){
+                console.log("Error on OzoneHandlers.tweets()" + err);
+                res.end("No tweets found at this time.");
+            }
+            else{
+                var tweetToHTML = "";
+                for (key in reply.results){
+                    tweetToHTML += "@"+reply.results[key].from_user +": "+reply.results[key].text+"... ";
+                }
+                //tweetToHTML += "</p>";
+                res.end(tweetToHTML);
+            }
+        })
+    }
+
+
+
     return {
         tweets: tweets,
-       
     }
 }();
 
